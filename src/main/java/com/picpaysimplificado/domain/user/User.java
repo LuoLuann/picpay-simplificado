20757d15818f52164a4415c6e80a974172f2c62a
LuoLuann <luo.luann@gmail.com>
2023-10-15 15:52:44 -0300

    feat: correcting importation in User entity

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,11 +1,12 @@
 package com.picpaysimplificado.domain.user;
+import com.picpaysimplificado.domain.user.UserType;
+
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.usertype.UserType;
 
 import java.math.BigDecimal;
 
